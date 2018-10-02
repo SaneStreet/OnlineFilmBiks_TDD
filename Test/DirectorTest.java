50e9ac4d7614c8760073cade15d8a72ab4f12ec0
Michael Hansen <michaelhansenit@live.dk>
2018-10-02 10:31:43 +0200

    Test Case - Director constructor og testDirector oprettet og testet (green bar)

--- Test/DirectorTest.java
+++ Test/DirectorTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DirectorTest {
 
+    @Test
+    void testDirector(){
+        //director = "Ron Howard"
+
+        //3. setup
+        Director dir = new Director("Ron Howard");
+
+        //2. Execution
+        String director = dir.director;
+
+        //1. Assert
+        assertEquals("Ron Howard", director);
+    }
 }
