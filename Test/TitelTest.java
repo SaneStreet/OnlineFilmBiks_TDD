92cb2868762429eab65a01b978020586ff71b674
Michael Hansen <michaelhansenit@live.dk>
2018-10-02 10:05:46 +0200

    Test commit - Titel constructor og testTitel oprettet (green bar)

--- Test/TitelTest.java
+++ Test/TitelTest.java
@@ -1,5 +1,19 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TitelTest {
+class TitelTest {
 
+    @Test
+    void testTitel(){
+        //titel = "Tester Jester"
+
+        //3. Setup
+        Titel titel1 = new Titel("Tester Jester");
+
+        //2. Execution
+        String titel = titel1.titel;
+
+        //1. Assert
+        assertEquals("Tester Jester", titel);
+    }
 }
