c8340eb261dcc5c66d935540618dbf2d156c2f59
Michael Hansen <michaelhansenit@live.dk>
2018-10-02 11:11:39 +0200

    Test Case - Billetpris constructor og testBilletPris og hentBilletPris er oprettet og testet (green bar)

--- Test/BilletprisTest.java
+++ Test/BilletprisTest.java
@@ -1,4 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BilletprisTest {
 
+    @Test
+    void testBilletPris(){
+        //billetPris = 15.0
+
+        //3. Setup
+        Billetpris billet = new Billetpris(15.0);
+
+        //2. Execution
+        double billetPris = billet.billetPris;
+
+        //1. Assert
+        assertEquals(15.0, billetPris);
+    }
+    @Test
+    void hentBilletPris(){
+        //billetPris = 45.25
+
+        //3. Setup
+        Billetpris billet = new Billetpris(45.25);
+
+        //2. Execution
+        double billetPris = billet.getBilletPris();
+
+        //1. Assert
+        assertEquals(45.25, billetPris);
+    }
 }
--- src/Billetpris.java
+++ src/Billetpris.java
@@ -1,2 +1,11 @@
 public class Billetpris {
+    public double billetPris;
+
+    public Billetpris(double billetPris) {
+        this.billetPris = billetPris;
+    }
+
+    public double getBilletPris() {
+        return billetPris;
+    }
 }
